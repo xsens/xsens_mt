825c16f90f0edd8a82ad2d47130847503caaf937
Frans Klaver <frans.klaver@xsens.com>
2013-01-25 14:13:45 +0100

    Catch the unused interface in the probe step
    Currently we capture the unused interface in the attach function. This
    results in an error report in the kernel logs. Since we actually expect
    one of the interfaces to be rejected here, we should capture this
    earlier during device probing.

--- xsens_mt.c
+++ xsens_mt.c
@@ -42,11 +42,26 @@ static const struct usb_device_id id_table[] = {
 };
 MODULE_DEVICE_TABLE(usb, id_table);
 
-static int xsens_mt_attach(struct usb_serial *serial)
+static int has_required_endpoints(const struct usb_host_interface *interface)
 {
-	if ((serial->num_bulk_in == 0) || (serial->num_bulk_out == 0))
-		return -ENODEV;
+	__u8 i;
+	int has_bulk_in = 0;
+	int has_bulk_out = 0;
 
+	for (i = 0; i < interface->desc.bNumEndpoints; ++i) {
+		if (usb_endpoint_is_bulk_in(&interface->endpoint[i].desc))
+			has_bulk_in = 1;
+		else if (usb_endpoint_is_bulk_out(&interface->endpoint[i].desc))
+			has_bulk_out = 1;
+	}
+
+	return has_bulk_in && has_bulk_out;
+}
+
+static int xsens_mt_probe(struct usb_serial *serial, const struct usb_device_id *id)
+{
+	if (!has_required_endpoints(serial->interface->cur_altsetting))
+		return -ENODEV;
 	return 0;
 }
 
@@ -60,7 +75,7 @@ static struct usb_serial_driver xsens_mt_device = {
 	.bulk_in_size = 512,
 	.bulk_out_size = 512,
 
-	.attach = xsens_mt_attach,
+	.probe = xsens_mt_probe,
 };
 
 static struct usb_serial_driver * const serial_drivers[] = {
